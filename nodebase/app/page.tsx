c156c51ed09fabb41301e1692b3c61058ed1a029
coolkid141222 <liaojiahaocug@gmail.com>
2025-11-22 16:46:35 +0800

    Add prefetch

--- nodebase/app/page.tsx
+++ nodebase/app/page.tsx
@@ -1,13 +1,23 @@
 // import { useEffect } from "react";
 import prisma from "@/lib/db"
+import { useTRPC } from "@/trpc/client"
+import { caller } from "@/trpc/server"
+import { dehydrate, HydrationBoundary, useQuery } from "@tanstack/react-query"
+import { Client } from "./client"
+import { getQueryClient } from "@/trpc/server"
+import { trpc } from "@/trpc/server"
+import { Suspense } from "react"
 const Page = async () => {
-  const users = await prisma.user.findMany();
-  
+  const users = await caller.getUsers();
+  const queryClient = getQueryClient();
+  void queryClient.prefetchQuery(trpc.getUsers.queryOptions());
 
-  return( 
-    <div className="text-red-300">
-      { JSON.stringify(users) }
-    </div>
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Suspense>
+        <Client/>
+      </Suspense>
+    </HydrationBoundary>
   )
 }
 
