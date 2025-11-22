5c32e4161ed2fb3551f50e7094f8355f6cf9dd98
coolkid141222 <liaojiahaocug@gmail.com>
2025-11-22 15:48:54 +0800

    Add prisma
    prisma database

--- nodebase/app/page.tsx
+++ nodebase/app/page.tsx
@@ -1,5 +1,14 @@
-const Page = () => {
-  <div>
-    hello
-  </div>
+// import { useEffect } from "react";
+import prisma from "@/lib/db"
+const Page = async () => {
+  const users = await prisma.user.findMany();
+  
+
+  return( 
+    <div className="text-red-300">
+      { JSON.stringify(users) }
+    </div>
+  )
 }
+
+export default Page;
